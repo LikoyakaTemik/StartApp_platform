47effa76e73ddc4f6b230c765d2f5a6c988e4270
Artem <bukanov.artem@mail.ru>
2022-10-16 16:05:03 +0300

    email-sender module

--- Backend/innovations/mail_sender.py
+++ Backend/innovations/mail_sender.py
@@ -3,16 +3,18 @@ import smtplib, ssl
 
 
 
-def sender():
+def sender(receiver):
     smtp_server = "smtp.gmail.com"
     port = 587  # For starttls
     sender_email = "likoydes@gmail.com"
-    receiver_email = "likoydes@gmail.com"
+    receiver_email = receiver
     password = "alaeacjrnnqowboa"
     message = ""
     for i in range(0, 6):
         ch = random.randint(1, 10)
         message = message + str(ch)
+    if(len(message) > 6):
+        message = message[0] + message[1] + message[2] + message[3] + message[4] + message[5]
     context = ssl.create_default_context()
     try:
         server = smtplib.SMTP(smtp_server,port)
@@ -28,5 +30,4 @@ def sender():
     finally:
         server.quit()
     return message
-
-sender()
+print(sender("bukanov.artem@mail.ru"))
